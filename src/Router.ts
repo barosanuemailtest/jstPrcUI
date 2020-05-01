df16f80b3500ebc48be9080d54aaf905358799e5
barosanuemailtest <barosanu.email.test@gmail.com>
2020-05-01 11:34:14 +0300

    Dinamically load html

--- src/Router.ts
+++ src/Router.ts
@@ -4,11 +4,18 @@ export class Router {
         return window.location.pathname;
     }
 
+    private setMainContainer(htmlPah: string): void {
+        document.getElementById('main-container')!.innerHTML = `<iframe src="${htmlPah}"></iframe>`;
+    }
+
     public handleRequest() {
         console.log('handling request for: ' + this.getRoute());
         switch (this.getRoute()) {
-
+            case '/login':
+                this.setMainContainer('./html/login.html')
+                break;
             default:
+                this.setMainContainer('./html/main.html')
                 break;
         }
     }
